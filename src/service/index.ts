b325ccd278e82151ebba61a2cead0938d4b8a938
Duc Thai <ducthai060501@gmail.com>
2022-12-21 23:40:55 +0200

    no more stuck, install some extra extension to pass cors

--- src/service/index.ts
+++ src/service/index.ts
@@ -4,10 +4,8 @@ export const DRONE_URL = 'http://assignments.reaktor.com/birdnest/drones' ;
 
 export const fetchData = async (url : string) => {
    try {
-    const res = await fetch(url, {
-        method: 'GET' ,
-    })
-    console.log(res.status) ; 
+    const res = await axios.get(url)
+    console.log(res) ; 
    } catch(e) {
     console.log(e)
    }
