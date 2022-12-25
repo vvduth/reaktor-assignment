1ea4e8cf93af6c8d3478e1fb0115fa804667dcc9
Duc Thai <ducthai060501@gmail.com>
2022-12-25 12:07:37 +0200

    xml parser and sned request per 2 secs

---
+++ src/assets/dummyData.ts
@@ -0,0 +1,86 @@
+export const  DUMMY_DATA = {
+    "report": {
+      "deviceInformation": {
+        "listenRange": 500000,
+        "deviceStarted": "2022-12-24T22:11:29.221Z",
+        "uptimeSeconds": 36353,
+        "updateIntervalMs": 2000
+      },
+      "capture": {
+        "drone": [
+          {
+            "serialNumber": "SN-jwsHQ5MOFC",
+            "model": "HRP-DRP 1",
+            "manufacturer": "ProDröne Ltd",
+            "mac": "38:94:66:3b:a3:ed",
+            "ipv4": "77.76.225.72",
+            "ipv6": "f4d2:bdce:1bcb:6f04:6240:061f:80e0:39ca",
+            "firmware": "0.5.4",
+            "positionY": 251615.52781743847,
+            "positionX": 162010.3720115924,
+            "altitude": 4186.765999298394
+          },
+          {
+            "serialNumber": "SN-gMqhv-RlQg",
+            "model": "HRP-DRP 1",
+            "manufacturer": "ProDröne Ltd",
+            "mac": "d2:57:ea:bb:f9:67",
+            "ipv4": "35.92.95.86",
+            "ipv6": "232f:bb01:6d78:edb3:4a74:345e:ee17:1250",
+            "firmware": "1.4.6",
+            "positionY": 476272.60547432693,
+            "positionX": 320934.9492176386,
+            "altitude": 4203.620104660799
+          },
+          {
+            "serialNumber": "SN-l1ajkV9DyU",
+            "model": "Altitude X",
+            "manufacturer": "DroneGoat Inc",
+            "mac": "e1:b0:04:68:c2:7d",
+            "ipv4": "127.118.231.46",
+            "ipv6": "3943:ee56:8d47:ecff:7f9a:f9bb:7f70:5cf6",
+            "firmware": "8.9.6",
+            "positionY": 106417.5055369206,
+            "positionX": 240270.95926860705,
+            "altitude": 4875.64766210184
+          },
+          {
+            "serialNumber": "SN-68C4tFlmwB",
+            "model": "Altitude X",
+            "manufacturer": "DroneGoat Inc",
+            "mac": "8e:2d:a0:4d:a1:cc",
+            "ipv4": "223.33.107.191",
+            "ipv6": "f89f:c5a4:af82:d2c5:2c2b:0881:694c:307f",
+            "firmware": "3.0.8",
+            "positionY": 188973.2942376041,
+            "positionX": 132868.19720750058,
+            "altitude": 4035.1658896739195
+          },
+          {
+            "serialNumber": "SN-kJo69KA0W_",
+            "model": "Mosquito",
+            "manufacturer": "MegaBuzzer Corp",
+            "mac": "cc:06:9f:45:5d:94",
+            "ipv4": "186.192.186.120",
+            "ipv6": "1260:6058:571f:10f2:4e0e:3aa6:d610:611e",
+            "firmware": "7.5.3",
+            "positionY": 214959.65179548028,
+            "positionX": 325948.6305828654,
+            "altitude": 4194.399219440139
+          },
+          {
+            "serialNumber": "SN-VO1skZd09d",
+            "model": "HRP-DRP 1 S",
+            "manufacturer": "ProDröne Ltd",
+            "mac": "5c:7c:49:b6:bd:24",
+            "ipv4": "154.130.157.117",
+            "ipv6": "8aea:7d44:9ecd:b79c:29ca:2bea:ee22:8193",
+            "firmware": "0.6.2",
+            "positionY": 77923.02512518047,
+            "positionX": 432017.59161406575,
+            "altitude": 4372.189331572973
+          }
+        ]
+      }
+    }
+  }
--- src/service/index.ts
+++ src/service/index.ts
@@ -1,13 +1,18 @@
 import axios from "axios";
+import XMLParser from 'react-xml-parser';
 
-export const DRONE_URL = 'http://assignments.reaktor.com/birdnest/drones' ;
+export const DRONE_URL = "http://assignments.reaktor.com/birdnest/drones";
 
-export const fetchData = async (url : string) => {
-   try {
-    const res = await axios.get(url)
-    console.log(res) ; 
-   } catch(e) {
-    console.log(e)
-   }
-}
-
+export const fetchData = async (url: string) => {
+  try {
+    const res = await axios.get(url, {
+      headers: {
+        "Content-Type": "application/xml; charset=utf-8",
+      },
+    });
+    let xml = new XMLParser().parseFromString(res.data)
+    return xml ;
+  } catch (e) {
+    console.log(e);
+  }
+};
